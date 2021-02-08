650e26db09638a3a2dfc50a049323d186e736cc1
Student <micahjgwin@gmail.com>
2021-02-08 11:51:23 -0600

    Added comments and memory management

--- 1.01/project1-01.c
+++ 1.01/project1-01.c
@@ -5,19 +5,21 @@
 int main(int argc, char *argv[]){
 
   char **world;
-
+  //Allocate space for each row
   world = (char **)malloc(WORLD_HEIGHT * sizeof(char *));
   int i;
+  //Allocate space based on size of row for all rows
   for(i = 0; i < WORLD_HEIGHT; i++){
     world[i] = (char *)malloc(WORLD_WIDTH * sizeof(char));
   }
   fill_edges(world);
   generate_world(world);
   print_world(world);
-  
+  clean_up(world);
 }
 
 void fill_edges(char **world){
+ //Add a border around edges
  int i ,j;
  for(j = 0; j < WORLD_HEIGHT; j++){
     for(i = 0; i < WORLD_WIDTH; i++){
@@ -32,6 +34,7 @@ void fill_edges(char **world){
 }
 
 void generate_world(char **world){
+  //Placeholder for now but world generation algorithm will go here
   int i, j;
   for(j = 1; j < WORLD_HEIGHT - 1; j++){
     for(i = 1; i < WORLD_WIDTH - 1; i++){
@@ -41,10 +44,21 @@ void generate_world(char **world){
 }
 
 void print_world(char **world){
+  //Display world in console (only displays correctly when terminal is 80x24)
   int i, j;
   for(j = 0; j < WORLD_HEIGHT; j++){
     for(i = 0; i < WORLD_WIDTH; i++){
       printf("%c", world[j][i]);
     }
   }
+}
+
+void clean_up(char **world){
+  //Memory management to avoid any leaks
+  int i;
+  for(i = 0; i < WORLD_HEIGHT; i++){
+    free(world[i]);
+  }
+
+  free(world);
 }

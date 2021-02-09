dff2512bf4b43778a7ce634a4a5162708fa7f070
Student <micahjgwin@gmail.com>
2021-02-09 00:41:54 -0600

    Added random room generation and placement

--- 1.01/project1-01.h
+++ 1.01/project1-01.h
@@ -1,6 +1,16 @@
 #define WORLD_WIDTH 80
 #define WORLD_HEIGHT 21
-void fill_edges(char **world);
-void generate_world(char **world);
-void print_world(char **world);
-void clean_up(char **world);
+#define MIN_ROOMS 6
+#define MAX_ROOMS 15
+#define MIN_ROOM_WIDTH 4
+#define MIN_ROOM_HEIGHT 3
+#define MAX_ROOM_WIDTH  8
+#define MAX_ROOM_HEIGHT 6
+#define ROOM_SPACE_BUFFER 3
+void generate_world();
+void place_rooms(int roomCount);
+int rooms_overlap(int xPos, int yPos, int width, int height, int buffer,int numRooms);
+int in_bounds(int xPos, int yPos, int width, int height);
+void fill_edges();
+void print_world();
+void clean_up();
